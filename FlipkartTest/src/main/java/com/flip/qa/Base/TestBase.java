5b25d4ff49045055fa30a9a9793fafd44bdfda0c
RJ <RJ@RAJESH>
2019-05-15 20:16:54 +0530

    chandra changes

--- FlipkartTest/src/main/java/com/flip/qa/Base/TestBase.java
+++ FlipkartTest/src/main/java/com/flip/qa/Base/TestBase.java
@@ -17,7 +17,7 @@ import com.flip.qa.Util.WebEventListner;
 public class TestBase {
 	
 	public static WebDriver driver;
-	public static WebDriver driver1;
+	public static int x=0;
 	public  static Properties prop;
 	  
 	 static EventFiringWebDriver  event_driver;
